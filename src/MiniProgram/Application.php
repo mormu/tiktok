23246b0e1fa365a25941c0a4d31cf714b3e35029
morMu <596129221@qq.com>
2022-07-07 16:23:16 +0800

    comit

---
+++ src/MiniProgram/Application.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace EasyTiktok\MiniProgram;
+
+use EasyTiktok\Kernel\ServiceContainer;
+use EasyTiktok\Kernel\Traits\ResponseCastable;
+
+/**
+ * Class Application.
+ * @property Base\Client $base
+ * @property Auth\AccessToken $access_token
+ * @property Auth\Client $auth
+ * @property QrCode\Client $qr_code
+ * @property Server\Encryptor $encryptor
+ * @author zhaoxiang <zhaoxiang051405@gmail.com>
+ */
+class Application extends ServiceContainer {
+
+    use ResponseCastable;
+
+    /**
+     * @var array
+     */
+    protected $providers = [
+        Base\ServiceProvider::class,
+        Auth\ServiceProvider::class,
+        QrCode\ServiceProvider::class,
+        Server\ServiceProvider::class
+    ];
+
+    /**
+     * 初始化小程序的基础接口
+     * @var array|\string[][]
+     */
+    protected $defaultConfig = [
+        'http' => [
+            'base_uri' => 'https://developer.toutiao.com/api/',
+        ],
+    ];
+
+    /**
+     * Handle dynamic calls.
+     *
+     * @param string $method
+     * @param array $args
+     *
+     * @return mixed
+     */
+    public function __call(string $method, array $args) {
+        return $this->base->$method(...$args);
+    }
+}
